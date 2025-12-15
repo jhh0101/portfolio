2e290b607a3a7e5706d273b92e5a1f99a7d4cc97
Haehun <jhh010143@gmail.com>
2025-12-15 16:29:59 +0900

    feat: 프로젝트 생성

---
+++ AuctionMarket/src/main/java/com/portfolio/auctionmarket/AuctionMarketApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.auctionmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionMarketApplication.class, args);
+    }
+
+}
