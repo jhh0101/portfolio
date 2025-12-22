ae16edc92039174accdb1522b7e27f46278cccf9
Haehun <jhh010143@gmail.com>
2025-12-22 21:11:03 +0900

    feat: 로그아웃 기능 추가

--- AuctionMarket/src/main/java/com/portfolio/auctionmarket/auth/controller/AuthController.java
+++ AuctionMarket/src/main/java/com/portfolio/auctionmarket/auth/controller/AuthController.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,6 +43,11 @@ public class AuthController {
             log.error("로그인 실패 : " + e.toString());
             throw new LoginException("로그인에 실패했습니다.");
         }
+    }
+
+    public ResponseEntity<ApiResponse<String>> logout(@AuthenticationPrincipal Long userId) {
+        authService.logout(userId);
+        return ResponseEntity.ok(ApiResponse.success("로그아웃 성공"));
     }
 
 }
