997eb49004be1caa6f7ef09b8fe2dcbaaf997c90
Haehun <jhh010143@gmail.com>
2025-12-24 00:40:58 +0900

    fix: base 분리
    createAt과 updateAt 분리

--- AuctionMarket/src/main/java/com/portfolio/auctionmarket/global/base/BaseCreatedAt.java
+++ AuctionMarket/src/main/java/com/portfolio/auctionmarket/global/base/BaseCreatedAt.java
@@ -5,16 +5,16 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+@EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 @Getter
-public abstract class Base extends BaseCreatedAt{
+public abstract class BaseCreatedAt {
 
-    @LastModifiedDate
-    @Column(name = "updated_at")
-    private LocalDateTime updatedAt;
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
 }
