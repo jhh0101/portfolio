3f42609612310bd88de1bef23c2fdf63e643b666
Haehun <jhh010143@gmail.com>
2025-12-25 00:22:15 +0900

    feat: 카테고리 조회 기능 추가

--- AuctionMarket/src/main/java/com/portfolio/auctionmarket/domain/categories/repository/CategoryRepository.java
+++ AuctionMarket/src/main/java/com/portfolio/auctionmarket/domain/categories/repository/CategoryRepository.java
@@ -4,6 +4,12 @@ import com.portfolio.auctionmarket.domain.categories.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    List<Category> findByParentIsNull();
+
+    List<Category> findByParent_CategoryId(Long parentId);
 }
