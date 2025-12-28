667287e639d67a89d068c13ed24bce3549fc0408
Haehun <jhh010143@gmail.com>
2025-12-28 19:20:21 +0900

    feat: LocalDateTime 포멧 추가

--- AuctionMarket/src/main/java/com/portfolio/auctionmarket/domain/auctions/dto/AuctionRequest.java
+++ AuctionMarket/src/main/java/com/portfolio/auctionmarket/domain/auctions/dto/AuctionRequest.java
@@ -1,5 +1,6 @@
 package com.portfolio.auctionmarket.domain.auctions.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -17,9 +18,11 @@ public class AuctionRequest {
     private Integer startPrice;
 
     @NotNull(message = "시작 기간을 입력해주세요.")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime startTime;
 
     @NotNull(message = "종료 기간을 입력해주세요.")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime endTime;
 
 }
