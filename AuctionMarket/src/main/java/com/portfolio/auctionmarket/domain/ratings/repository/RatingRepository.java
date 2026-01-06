ff91c10cd6a5923299f6a78dad8b2540dc9a21f9
Haehun <jhh010143@gmail.com>
2026-01-06 23:00:24 +0900

    fix: 중복 평가 불가능하게 변경

--- AuctionMarket/src/main/java/com/portfolio/auctionmarket/domain/ratings/repository/RatingRepository.java
+++ AuctionMarket/src/main/java/com/portfolio/auctionmarket/domain/ratings/repository/RatingRepository.java
@@ -1,9 +1,11 @@
 package com.portfolio.auctionmarket.domain.ratings.repository;
 
+import com.portfolio.auctionmarket.domain.orders.entity.Order;
 import com.portfolio.auctionmarket.domain.ratings.entity.Rating;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RatingRepository extends JpaRepository<Rating, Long> {
+    boolean existsByOrder(Order order);
 }
