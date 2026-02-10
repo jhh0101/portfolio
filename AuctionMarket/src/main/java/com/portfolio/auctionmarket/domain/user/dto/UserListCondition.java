d1c54e81eab468d47b11a808ac0b9f81f2ef7b91
Haehun <jhh010143@gmail.com>
2026-02-10 21:15:07 +0900

    feat: 권한으로 검색하는 기능 추가 (#102)

--- AuctionMarket/src/main/java/com/portfolio/auctionmarket/domain/user/dto/UserListCondition.java
+++ AuctionMarket/src/main/java/com/portfolio/auctionmarket/domain/user/dto/UserListCondition.java
@@ -1,5 +1,6 @@
 package com.portfolio.auctionmarket.domain.user.dto;
 
+import com.portfolio.auctionmarket.domain.user.entity.Role;
 import com.portfolio.auctionmarket.domain.user.entity.UserStatus;
 
-public record UserListCondition(String email, String nickname, UserStatus status) {}
+public record UserListCondition(String email, String nickname, UserStatus status, Role role) {}
