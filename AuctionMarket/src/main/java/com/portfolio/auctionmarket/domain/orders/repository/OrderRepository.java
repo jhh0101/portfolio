f35638bed67ccbd70b3c3e806f5c807af117bd43
Haehun <jhh010143@gmail.com>
2026-02-15 00:49:48 +0900

    feat: 사용자의 낙찰 리스트 조회(관리자 전용)

--- AuctionMarket/src/main/java/com/portfolio/auctionmarket/domain/orders/repository/OrderRepository.java
+++ AuctionMarket/src/main/java/com/portfolio/auctionmarket/domain/orders/repository/OrderRepository.java
@@ -7,11 +7,13 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
     @EntityGraph(attributePaths = {"auction", "auction.product", "auction.product.image"})
     Page<Order> findAllByBuyer_UserId(Long userId, Pageable pageable);
 
-    Order findAllByAuction_AuctionId(Long auctionAuctionId);
+    Optional<Order> findByAuction_AuctionId(Long auctionAuctionId);
 }
